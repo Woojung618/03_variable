44ff072c9fc2eed71a7fd486552df5cfc780b244
Woojung618 <eyes_cat@naver.com>
2019-09-19 15:29:56 +0900

    size

--- main.c
+++ main.c
@@ -4,6 +4,12 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	printf("Hello Sookmyung EE!\n");
+	
+	int x; short y; char w; double d; float f;
+	printf("bytes of int : %d\n",sizeof(int));
+	printf("bytes of short : %d\n",sizeof(short));
+	printf("bytes of char : %d\n",sizeof(char));
+	printf("bytes of double : %d\n",sizeof(double));
+	printf("bytes of flaot : %d\n",sizeof(float));
 	return 0;
 }
